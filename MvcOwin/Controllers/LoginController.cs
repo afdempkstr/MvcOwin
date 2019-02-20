ee6301fde145e20e1ace03afcaa452668c22af9f
Kostas Stroggylos <kostas.stroggylos@peoplecert.onmicrosoft.com>
2019-02-20 21:27:44 +0200

    login controller

--- MvcOwin/Controllers/LoginController.cs
+++ MvcOwin/Controllers/LoginController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
 using MvcOwin.Models;
 using MvcOwin.ViewModels;
 
@@ -20,19 +23,35 @@ namespace MvcOwin.Controllers
         public ActionResult Login(Account account)
         {
             UserManager manager = new UserManager();
+            var user = manager.GetUser(account.Username);
+            if (user == null || user.Password != account.Password)
+            {
+                ModelState.AddModelError("Username", "Invalid username or password");
+                return RedirectToAction("Index");
+            }
 
-            //var loggedInUser = manager.Login(account.Username, account.Password);
+            var claims = new List<Claim>(new[]
+            {
+                // adding following 2 claim just for supporting default antiforgery provider
+                new Claim(ClaimTypes.NameIdentifier, user.Username),
+                new Claim(
+                    "http://schemas.microsoft.com/accesscontrolservice/2010/07/claims/identityprovider",
+                    "ASP.NET Identity", "http://www.w3.org/2001/XMLSchema#string"),
+                new Claim(ClaimTypes.Name, user.Username)
+            });
 
-            //if (loggedInUser != null)
-            //{
-            //    Session["user"] = loggedInUser;
-            //    return View("Success", loggedInUser);
-            //}
-            //else
-            //{
-            //    return RedirectToAction("Index");
-            //}
-            return RedirectToAction("Index");
+            foreach (var role in user.Roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role.Name));
+            }
+
+            var identity = new ClaimsIdentity(claims, DefaultAuthenticationTypes.ApplicationCookie);
+
+            HttpContext.GetOwinContext().Authentication.SignIn(
+                new AuthenticationProperties { IsPersistent = false }, identity);
+
+            Session["user"] = user;
+            return RedirectToAction("Index", "Home");
         }
     }
 }
